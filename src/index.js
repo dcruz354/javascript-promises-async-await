70dfc668f57552b28e1424d7a7b41cfbdbec56cb
dcruz354 <dcruz354@gmail.com>
2022-11-01 15:50:54 -0700

    module 3
    module 3

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-export function fetchMovies() {
+import { fetchWithTimeout } from './services';
 
+var movies = require(movies.json);
+
+export function fetchMovies() {
+    const resolveFunction = () => movies;
+    return fetchWithTimeout(1000).then(resolveFunction)
 }
+
+var moviePromise = fetchMovies();
+
+console.log(moviePromise.then(results));
