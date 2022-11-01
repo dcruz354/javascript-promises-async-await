0efb7bd90619a4706ad461154e36a1e388288b5f
dcruz354 <dcruz354@gmail.com>
2022-11-01 15:56:33 -0700

    module 3 fix
    module 3 fix

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import { fetchWithTimeout } from './services';
 
-var movies = require(movies.json);
+const movies = require('./data/movies.json');
 
 export function fetchMovies() {
     const resolveFunction = () => movies;
     return fetchWithTimeout(1000).then(resolveFunction)
 }
 
-var moviePromise = fetchMovies();
+const moviePromise = fetchMovies();
 
-console.log(moviePromise.then(results));
+moviePromise.then(results => {
+    console.log(results);
+});
